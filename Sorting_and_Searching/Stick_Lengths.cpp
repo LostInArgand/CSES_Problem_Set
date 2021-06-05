94e3c679ec6894a4c2a17dd6527902aec5673481
LostInArgand <39057735+LostInArgand@users.noreply.github.com>
2021-06-05 22:16:14 +0530

    Create Stick_Lengths.cpp

---
+++ Sorting_and_Searching/Stick_Lengths.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <climits>
+#include <unordered_map>
+#include <cmath>
+#include <vector>
+#include <unordered_set>
+#include <queue>
+
+typedef long long int ll;
+//typedef    unordered_map<int, int> umap;
+//typedef    unordered_set<int> uset;
+
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define testcase(T) for(int t = 1; t <= T; t++)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define uset unordered_set<int>
+#define unmap unordered_map<int, int>
+#define MOD 1000000007
+
+
+using namespace std;
+
+
+int max(int x, int y){
+	if (x > y){
+		return x;
+		}
+	return y;
+	}
+	
+int min(int x, int y){
+	if (x <= y){
+		return x;
+		}
+	return y;
+	}
+
+ll fastExpo(ll i, ll j, ll mod){
+	ll res = 1;
+	i = i % mod;
+	if(j == 0){
+		return 1;
+		}
+	if(i == 0){
+		return 0;
+		}
+	while (j > 0){
+		if((j & 1) == 1){
+			res = (res * i) % mod;
+			}
+		j >>= 1;
+		i = (i * i) % mod;
+		}
+	return res;
+	}
+
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int n;
+	cin >> n;
+	ll arr[n];
+	for(int i = 0; i < n; i++){
+		cin >> arr[i];
+		}
+	sort(arr, arr + n);
+	ll middle = arr[(ll)ceil(((double)n)/2.0) - 1];
+	ll ans = 0;
+	for(int i = 0; i < n; i++){
+		ans += abs(arr[i] - middle);
+		}
+	cout << ans;
+}
