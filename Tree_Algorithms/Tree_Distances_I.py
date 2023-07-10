5a26c59a9512892e233352ac062336530981d101
LostInArgand <39057735+LostInArgand@users.noreply.github.com>
2023-07-10 19:35:09 +0530

    Create Tree_Distances_I.py

---
+++ Tree_Algorithms/Tree_Distances_I.py
@@ -0,0 +1,48 @@
+from collections import defaultdict, deque
+import sys
+
+sys.setrecursionlimit(200010)
+
+class Graph:
+    def __init__(self, V):
+        self.V = V
+        self.graph = defaultdict(list)
+        self.visited = set()
+        self.maxx = -1
+        self.X = None
+        self.dists = [0] * self.V
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def bfs(self, u):
+        q = deque()
+        q.append((u, 0))
+        while q:
+            u, dist = q.popleft()
+            if self.maxx < dist:
+                self.maxx = dist
+                self.X = u
+            self.dists[u - 1] = max(dist, self.dists[u - 1])
+            self.visited.add(u)
+            for v in self.graph[u]:
+                if v not in self.visited:
+                    q.append((v, dist + 1))
+
+    def solve(self):
+        self.bfs(1)
+        self.visited = set()
+        self.bfs(self.X)
+        self.visited = set()
+        self.bfs(self.X)
+        return self.dists
+
+
+n = int(input())
+graph = Graph(n)
+for i in range(n - 1):
+    u, v = map(int, input().split())
+    graph.addEdge(u, v)
+print(' '.join(map(str, graph.solve())))
+
